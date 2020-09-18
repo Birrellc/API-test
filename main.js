e11857aac2bdd90cecbfe401d04c2538c63308ab
Chris Birrell <vaiezmedia@gmail.com>
2020-09-18 14:08:37 +0100

    updated

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ getData(function(data){
     console.log(data);
 }) */
 
-// CALL BACK METHOD 2
+/* CALL BACK METHOD 2
 
 function getData(cb) {
     var xhr = new XMLHttpRequest();
@@ -70,3 +70,55 @@ function printDataToConsole(data) {
 }
 
 getData(printDataToConsole);
+
+*/
+
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        var tableRows = [];
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            // el.innerHTML += "<p>" + item.name + "</p>";
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key){
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
