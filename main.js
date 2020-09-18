aba54ed76f76cc953e851363ee3b9310730a14e5
Chris Birrell <vaiezmedia@gmail.com>
2020-09-18 13:20:24 +0100

    update x

--- main.js
+++ main.js
@@ -1,11 +1,72 @@
-var xhr = new XMLHttpRequest();
+/* TIMEOUT METHOD
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
-    }
-};
+var xhr = new XMLHttpRequest();
+var data;
 
 xhr.open("GET", "http://ci-swapi.herokuapp.com/api/");
+xhr.send();
 
-xhr.send(); 
+function setData(jsonData) {
+    data = jsonData;
+}
+
+xhr.onreadystatechange = function() {
+    
+    if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText); 
+        
+    }
+};
+setTimeout(function(){
+    console.log(data);
+}, 500);
+
+*/
+
+/* CALL BACK METHOD 1 
+
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+
+
+
+    xhr.open("GET", "http://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+
+            if (this.readyState == 4 && this.status == 200) {
+                cb(JSON.parse(this.responseText));
+
+            }
+        };
+    }
+
+getData(function(data){
+    console.log(data);
+}) */
+
+// CALL BACK METHOD 2
+
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+
+
+
+    xhr.open("GET", "http://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+
+            if (this.readyState == 4 && this.status == 200) {
+                cb(JSON.parse(this.responseText));
+
+            }
+        };
+    }
+
+function printDataToConsole(data) {
+    console.log(data);
+}
+
+getData(printDataToConsole);
